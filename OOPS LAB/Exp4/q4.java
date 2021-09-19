ed99bd7d879b2026d4026b830c047e003abdd777
Tihsrah <67807269+Tihsrah@users.noreply.github.com>
2021-09-19 17:44:18 +0530

    Delete q4.java

--- OOPS LAB/Exp4/q4.java
+++
@@ -1,7 +0,0 @@
-//4.Consider the trunk calls of a telephone exchange. A trunk call can be ordinary,
-// urgent or lightning. The charges depend on the duration and the type of the call.
-// Write a program using the concept of polymorphism in Java to calculate the charges.
-
-public class q4 {
-    
-}
