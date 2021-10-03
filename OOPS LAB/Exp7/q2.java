45cf0ecf61624a32a46bdfa34366d7e7481c28a4
Tihsrah <67807269+Tihsrah@users.noreply.github.com>
2021-10-03 23:59:21 +0530

    Update q2.java

--- OOPS LAB/Exp7/q2.java
+++ OOPS LAB/Exp7/q2.java
@@ -10,9 +10,6 @@ public class q2 {
             char t = args[1].charAt(0);
             if (!(Character.isDigit(t)))
                 throw new MyException(" Not an Integer");
-
-            int x = Integer.parseInt(args[0]);
-            // int y= Integer.
         } catch (MyException e) {
             System.out.println(e.getMessage());
         }
